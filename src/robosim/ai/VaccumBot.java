4b82efdb075e03bb53b04425c472413c2f83de96
JCT03 <119342826+JCT03@users.noreply.github.com>
2023-09-05 21:38:56 -0500

    Final Touches

--- src/robosim/ai/VaccumBot.java
+++ src/robosim/ai/VaccumBot.java
@@ -8,7 +8,7 @@ import javax.swing.text.html.Option;
 import java.util.Optional;
 
 public class VaccumBot implements Controller {
-    public QTable qTable = new QTable(7, 4, 0, 5, 1, 0.5);
+    public QTable qTable = new QTable(7, 4, 0, 5, 2, 0.5);
     // States:
     // 0: Bumped
     // 1: DirtFound
@@ -30,14 +30,15 @@ public class VaccumBot implements Controller {
             dirt = sim.getDirt();
             NextAction = qTable.senseActLearn(1, 10);
         }
-        else if(dirtObj.isPresent() && dirtObj.get().getR() > closestBad){
-            if(closestBad > 30)
-                NextAction = qTable.senseActLearn(3, 0);
-            else
-                NextAction = qTable.senseActLearn(2, 0);
-        } else if(dirtObj.isPresent()) {
+        else if (sim.wasHit()) {
+            NextAction = qTable.senseActLearn(0, -5);
+        }
+        else if(dirtObj.isPresent() && dirtObj.get().getR() > closestBad && closestBad <30){
+            NextAction = qTable.senseActLearn(2, 0);
+        } 
+        else if(dirtObj.isPresent()) {
             double angle = Math.abs(dirtObj.get().getTheta()) - Robot.ANGULAR_VELOCITY;
-            if (Math.abs(angle) < 5) {
+            if (Math.abs(angle) < 2) {
                 NextAction = qTable.senseActLearn(4, 0);
             } else if (angle < 0) {
                 NextAction = qTable.senseActLearn(5, 0);
@@ -45,35 +46,16 @@ public class VaccumBot implements Controller {
                 NextAction = qTable.senseActLearn(6, 0);
             }
         } else {
-            NextAction = qTable.senseActLearn(0, 0);
-        }
-
-    }
-    public Optional<Action> dirtAction(Simulator sim) {
-        int leftDirt = 0;
-        int rightDirt = 0;
-        int straightDirt = 0;
-        for (Duple<SimObject, Polar> obj: sim.allVisibleObjects()) {
-            if (obj.getFirst().isVacuumable()) {
-                if (Math.abs(obj.getSecond().getTheta()) < Robot.ANGULAR_VELOCITY) {
-                    straightDirt += 1;
-                } else if (obj.getSecond().getTheta() < 0) {
-                    leftDirt += 1;
-                } else {
-                    rightDirt += 1;
-                }
+            if (qTable.getLastAction() == 0) {
+                NextAction = qTable.senseActLearn(3, 1);
+            } else {
+                NextAction = qTable.senseActLearn(3, 0);
             }
         }
-        if (straightDirt > 0) {
-            return Optional.of(Action.FORWARD);
-        } else if (leftDirt > 0) {
-            return Optional.of(Action.LEFT);
-        } else if (rightDirt > 0) {
-            return Optional.of(Action.RIGHT);
-        } else {
-            return Optional.empty();
-        }
+        actions[NextAction].applyTo(sim);
+
     }
+
 //    public Duple<SimObject, Polar> findClosestObject(Simulator sim) {
 //        Duple<SimObject, Polar> closestObject = null;
 //        for (Duple<SimObject, Polar> obj: sim.allVisibleObjects()) {
