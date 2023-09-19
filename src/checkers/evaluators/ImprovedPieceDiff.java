a3565aeaf4d11ee80c48fb846e47e6a273c543fc
TMGBNoodle <grovesbenedicttm@hendrix.edu>
2023-09-18 20:30:16 -0500

    Create ImprovedPieceDiff.java

---
+++ src/checkers/evaluators/ImprovedPieceDiff.java
@@ -0,0 +1,14 @@
+package checkers.evaluators;
+
+import checkers.core.Checkerboard;
+
+import java.util.function.ToIntFunction;
+
+public class ImprovedPieceDiff implements ToIntFunction<Checkerboard> {
+    @Override
+    public int applyAsInt(Checkerboard value) {
+        int result = value.numPiecesOf(value.getCurrentPlayer())- value.numPiecesOf(value.getCurrentPlayer().opponent());
+        result = result + value.numKingsOf(value.getCurrentPlayer());
+        return result;
+    }
+}
