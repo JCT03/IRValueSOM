a7979366dcf7f0b758e2964a4100b1623c1f6ea2
TMGBNoodle <grovesbenedicttm@hendrix.edu>
2023-09-19 14:20:27 -0500

    SomeStuff

---
+++ src/checkers/searchers/AlphaBetaImproved.java
@@ -0,0 +1,85 @@
+package checkers.searchers;
+
+import checkers.core.Checkerboard;
+import checkers.core.CheckersSearcher;
+import checkers.core.Move;
+import core.Duple;
+
+import java.security.DrbgParameters;
+import java.util.ArrayList;
+import java.util.Dictionary;
+import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Enumeration;
+import java.util.Optional;
+import java.util.function.ToIntFunction;
+
+public class AlphaBetaImproved extends CheckersSearcher {
+    //    private ToIntFunction<Checkerboard> eval;
+//    private int maxDepth;
+    private int numNodes;
+
+    private int gameDepth;
+
+    private HashMap<Integer, HashMap<Integer, Checkerboard>> gameRecords;
+    @Override
+    public int numNodesExpanded() {
+        return numNodes;
+    }
+
+    @Override
+    public Optional<Duple<Integer, Move>> selectMove(Checkerboard board) {
+        gameDepth = board.getNumMovesMade();
+        return Optional.of(AlphaBetaFunc(board, getDepthLimit(), -Integer.MAX_VALUE, Integer.MAX_VALUE));
+    }
+
+    public AlphaBetaImproved(ToIntFunction<Checkerboard> e){
+        super(e);
+        gameRecords = new HashMap<Integer, HashMap<Integer, Checkerboard>>();
+    }
+
+    public Duple<Integer, Move> AlphaBetaFunc(Checkerboard board, int depthLimit, int alpha, int beta){
+        if(board.gameOver()){
+            if(board.playerWins(board.getCurrentPlayer())){
+//                gameRecords.put(Integer.MAX_VALUE, )
+                return new Duple<Integer, Move>(Integer.MAX_VALUE, null);
+            } else if(board.playerWins(board.getCurrentPlayer().opponent())){
+                return new Duple<Integer, Move>(-Integer.MAX_VALUE, null);
+            }
+        } else if(depthLimit <= 0) {
+            return new Duple<Integer, Move>(getEvaluator().applyAsInt(board), null);
+        }
+        int best_score = -Integer.MAX_VALUE;
+        Move best_move = null;
+        for (Move currentMove: board.getCurrentPlayerMoves()) {
+            numNodes +=1;
+            Checkerboard newBoard = board.duplicate();
+            newBoard.move(currentMove);
+            int value = 0;
+            Move move;
+            if (!newBoard.turnIsRepeating()) {
+                Duple<Integer, Move> nega = AlphaBetaFunc(newBoard, depthLimit - 1, -beta, -alpha);
+                value = -nega.getFirst();
+            } else{
+                Duple<Integer, Move> nega = AlphaBetaFunc(newBoard, depthLimit - 1, alpha, beta);
+                value = nega.getFirst();
+            }
+            if(best_move == null){
+                best_move = currentMove;
+                best_score = value;
+                alpha = value;
+            }
+            if(value > best_score){
+                best_move = currentMove;
+                best_score = value;
+                alpha = value;
+            }
+            if(alpha >= beta){
+                best_move = currentMove;
+                best_score = value;
+                return new Duple<Integer, Move>(best_score, best_move);
+            }
+        }
+        return new Duple<Integer, Move>(best_score, best_move);
+    }
+}
