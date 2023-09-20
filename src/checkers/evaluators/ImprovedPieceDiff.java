a837f445595a7071d5a9368828081d58e29b5c6f
JCT03 <119342826+JCT03@users.noreply.github.com>
2023-09-19 20:05:01 -0500

    updated improved evaluater

--- src/checkers/evaluators/ImprovedPieceDiff.java
+++ src/checkers/evaluators/ImprovedPieceDiff.java
@@ -7,8 +7,10 @@ import java.util.function.ToIntFunction;
 public class ImprovedPieceDiff implements ToIntFunction<Checkerboard> {
     @Override
     public int applyAsInt(Checkerboard value) {
-        int result = value.numPiecesOf(value.getCurrentPlayer())- value.numPiecesOf(value.getCurrentPlayer().opponent());
-        result = result + value.numKingsOf(value.getCurrentPlayer())-value.numKingsOf(value.getCurrentPlayer().opponent());
+        int pieceWeight= 100;
+        int kingWeight=200;
+        int result = pieceWeight * (value.numPiecesOf(value.getCurrentPlayer())- value.numPiecesOf(value.getCurrentPlayer().opponent()));
+        result += (kingWeight-pieceWeight) * (value.numKingsOf(value.getCurrentPlayer())-value.numKingsOf(value.getCurrentPlayer().opponent()));
         return result;
     }
 }
