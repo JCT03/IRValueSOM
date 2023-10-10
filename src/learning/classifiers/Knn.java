9b86adbdde88abe94c5fef6b82e176133180a90d
JCT03 <119342826+JCT03@users.noreply.github.com>
2023-10-10 13:23:25 -0500

    Completed knn

--- src/learning/classifiers/Knn.java
+++ src/learning/classifiers/Knn.java
@@ -24,11 +24,39 @@ public class Knn<V, L> implements Classifier<V, L> {
     public L classify(V value) {
         // TODO: Find the distance from value to each element of data. Use Histogram.getPluralityWinner()
         //  to find the most popular label.
-        return null;
+        Comparator<Duple<V, L>> dataComparator = new Comparator<Duple<V, L>>() { //https://www.callicoder.com/java-priority-queue/
+        @Override
+        public int compare(Duple<V, L> a, Duple<V, L> b) {
+            if (distance.applyAsDouble(a.getFirst(), value) > distance.applyAsDouble(b.getFirst(), value)) {
+                return 1;
+            }
+            else if (distance.applyAsDouble(a.getFirst(), value) < distance.applyAsDouble(b.getFirst(), value)) {
+                return -1;
+            }
+            else {
+                return 0;
+            }
+        }
+        };
+        PriorityQueue<Duple<V, L>> sortedV = new PriorityQueue<>(dataComparator);
+        for (Duple<V, L> dataPoint: data) {
+            sortedV.add(dataPoint);
+        }
+        Histogram<L> h = new Histogram<>();
+        System.out.println("value " +value);
+        for (int i = 0; i <k; i++) {
+            System.out.println(sortedV.peek());
+            h.bump(sortedV.poll().getSecond());
+        }
+        System.out.println(h);
+        return h.getPluralityWinner();
     }
 
     @Override
     public void train(ArrayList<Duple<V, L>> training) {
         // TODO: Add all elements of training to data.
+        for (Duple<V, L> dataPiece: training) {
+            data.add(dataPiece);
+        }
     }
 }
