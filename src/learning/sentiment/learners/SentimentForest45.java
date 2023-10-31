80b18bcc9012b778cb8be672a39770d1c7fcdc58
JCT03 <119342826+JCT03@users.noreply.github.com>
2023-10-30 22:49:45 -0500

    final push

--- src/learning/sentiment/learners/SentimentForest45.java
+++ src/learning/sentiment/learners/SentimentForest45.java
@@ -4,7 +4,7 @@ import learning.core.Histogram;
 import learning.decisiontree.RandomForest;
 import learning.sentiment.core.SentimentAnalyzer;
 
-public class SentimentForest30 extends RandomForest<Histogram<String>, String, String, Integer> {
+public class SentimentForest45 extends RandomForest<Histogram<String>, String, String, Integer> {
     public SentimentForest45() {
         super(45, SentimentAnalyzer::allFeatures, Histogram::getCountFor, i -> i + 1);
     }
